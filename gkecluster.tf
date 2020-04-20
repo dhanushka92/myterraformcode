f06bf69919738f38972fab8dad0379ba8981e79c
dhanushka92 <54582030+dhanushka92@users.noreply.github.com>
2020-04-20 17:10:06 +0530

    Update gkecluster.tf

--- gkecluster.tf
+++ gkecluster.tf
@@ -1,6 +1,2 @@
-resource "google_container_cluster" "gke-cluster" {
-  name               = "my-first-gke-cluster"
-  network            = "default"
-  zone               = "europe-west1-b"
-  initial_node_count = 3
+resource "aws_vpc" "myvpc" {
 }
