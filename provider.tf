6eae2be81b9d480c4b94ec9c097a26c4d05cabd8
dhanushka92 <54582030+dhanushka92@users.noreply.github.com>
2020-04-22 09:54:07 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
-provider "google" {
-  credentials = "${file("./creds/serviceaccount.json")}"
-  project     = "gke-tf-demo"
-  region      = "europe-west1"
+provider "aws" {
+  access key= "AKIAI2CH655DYDV4VZVQ"
+  secret_key = "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"
+  region      = "us-east-1"
 }
