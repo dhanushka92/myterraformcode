42919162077e88aeb5709d4289612e9dce9e8704
dhanushka92 <54582030+dhanushka92@users.noreply.github.com>
2020-04-28 15:16:18 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,7 @@
 resource "aws_vpc" "myvpc" {
   cidr_block="10.0.0.0/16"
 }
-
-resource "aws_instance" "appserver1"{
+resource "aws_instance" "server1"{
 ami="ami-08bf5f54919fada4a"
 instance_type="t2.micro"
 }
