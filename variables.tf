e0a68bf1624feb20e6cf37ebcede1381d6603224
dhanushka92 <54582030+dhanushka92@users.noreply.github.com>
2020-04-30 21:16:11 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "vpc_cidr"{
-default="10.0.0.0/8"
+default="10.0.0.0/24"
 }
 
 variable "inst_type"{
@@ -20,20 +20,20 @@ default="14.00.3223.3.v1"
 
 
 variable "prvt_subnet1_cidr"{
-default="10.0.0.0/10"
+default="10.0.0.0/26"
 }
 
 variable "prvt_subnet2_cidr"{
-default="10.64.0.0/10"
+default="10.0.0.64/26"
 }
 
 variable "prvt_subnet3_cidr"{
-default="10.128.0.0/10"
+default="10.0.0.128/26"
 }
 
-variable "prvt_subnet4_cidr"{
+/*variable "prvt_subnet4_cidr"{
 default="10.192.0.0/11"
-}
+}*/
 
 
 variable "ami"{
