8110dab43622533a27f84da9d918241da2aa65aa
dhanushka92 <54582030+dhanushka92@users.noreply.github.com>
2020-04-30 12:24:03 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,64 @@
-resource "aws_vpc" "myvpc" {
-  cidr_block="10.0.0.0/16"
+data "aws_availability_zones" "available" {
+  state = "available"
 }
-resource "aws_instance" "server1"{
-ami="ami-08bf5f54919fada4a"
-instance_type="t2.micro"
+
+resource "aws_vpc"  "sample_vpc"{
+cidr_block="${var.vpc_cidr}"
+tags = {
+    name = "VPC"
+  }
 }
+
+resource "aws_subnet" "subnet1"{
+vpc_id="${aws_vpc.sample_vpc.id}"
+cidr_block="${var.prvt_subnet1_cidr}"
+availability_zone="${data.aws_availability_zones.available.names[0]}"
+tags = {
+    name = "SUBNET1"
+  }
+}
+
+resource "aws_subnet" "subnet2"{
+vpc_id="${aws_vpc.sample_vpc.id}"
+cidr_block="${var.prvt_subnet2_cidr}"
+availability_zone="${data.aws_availability_zones.available.names[1]}"
+tags = {
+    name = "SUBNET2"
+  }
+}
+
+resource "aws_subnet" "subnet3"{
+vpc_id="${aws_vpc.sample_vpc.id}"
+cidr_block="${var.prvt_subnet3_cidr}"
+availability_zone="${data.aws_availability_zones.available.names[0]}"
+tags = {
+    name = "SUBNET3"
+  }
+}
+
+resource "aws_subnet" "subnet4"{
+vpc_id="${aws_vpc.sample_vpc.id}"
+cidr_block="${var.prvt_subnet4_cidr}"
+availability_zone="${data.aws_availability_zones.available.names[1]}"
+tags = {
+    name = "SUBNET4"
+  }
+}
+
+resource "aws_security_group" "sgrp1"{
+name="sgrp1"
+vpc_id="${aws_vpc.sample_vpc.id}"
+ ingress{
+ from_port=22
+ to_port=22
+ protocol="tcp"
+ cidr_blocks=["10.0.0.0/27"]
+}
+tags = {
+    name = "SECURITYGRP1"
+}
+}
+
+
+
+
