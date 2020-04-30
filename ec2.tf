149e4e6cf6331bfc9edb76c1d63bb24abf8cdf09
dhanushka92 <54582030+dhanushka92@users.noreply.github.com>
2020-04-30 21:54:13 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -14,7 +14,10 @@ instance_type="${var.inst_type}"
 subnet_id="${aws_subnet.subnet1.id}"
 vpc_security_group_ids = ["${aws_security_group.sgrp1.id}"]
 iam_instance_profile = "${aws_iam_instance_profile.test_profile.name}"
-user_data = "${file("chrome.ps1")}"
+provisioner "local-exec" {
+    command = <<EOH
+curl https://resourcepkgs.s3.amazonaws.com/ChromeSetup.exe
+EOH
 key_name="dhanu1"
 tags = {
     name = "INSTNCE1"
@@ -29,7 +32,11 @@ subnet_id="${aws_subnet.subnet2.id}"
 vpc_security_group_ids = ["${aws_security_group.sgrp1.id}"]
 iam_instance_profile = "${aws_iam_instance_profile.test_profile.name}"
 key_name="dhanu1"
-user_data = "${file("chrome.ps1")}"
+user_data     = <<EOF
+<powershell>
+
+</powershell>
+EOF
 tags = {
     name = "INSTANCE2"
   }
