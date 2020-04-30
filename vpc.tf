81143284705ad30b91de635b582a5bec340be458
dhanushka92 <54582030+dhanushka92@users.noreply.github.com>
2020-04-30 20:37:42 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -10,16 +10,16 @@ tags = {
 }
 resource "aws_subnet" "public-sub"{
 cidr_block="10.0.1.0/26"
-vpc_id="${aws_vpc.main.id}"
+vpc_id="${aws_vpc.sample_vpc.id}"
 availability_zone="us-east-1a"
 }
 
 resource "aws_internet_gateway" "igw"{
-vpc_id="${aws_vpc.main.id}"
+vpc_id="${aws_vpc.sample_vpc.id}"
 }
 
 resource "aws_route_table" "rtbl"{
-vpc_id="${aws_vpc.main.id}"
+vpc_id="${aws_vpc.sample_vpc.id}"
 route{
 cidr_block="0.0.0.0/0"
 gateway_id="${aws_internet_gateway.igw.id}"
