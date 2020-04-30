bf70c9efc581215058c07ba31f6c45425f0efab3
dhanushka92 <54582030+dhanushka92@users.noreply.github.com>
2020-04-30 12:26:24 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,49 @@
+variable "vpc_cidr"{
+default="10.0.0.0/24"
+}
+
+variable "inst_type"{
+default="t2.micro"
+}
+
+variable "inst_class"{
+default="db.t2.micro"
+}
+
+variable "eng"{
+default="sqlserver-ex"
+}
+
+variable "sqlver"{
+default="14.00.3223.3.v1"
+}
+
+
+variable "prvt_subnet1_cidr"{
+default="10.0.0.0/26"
+}
+
+variable "prvt_subnet2_cidr"{
+default="10.0.0.64/26"
+}
+
+variable "prvt_subnet3_cidr"{
+default="10.0.0.128/26"
+}
+
+variable "prvt_subnet4_cidr"{
+default="10.0.0.192/26"
+}
+
+
+variable "ami"{
+default="ami-08bf5f54919fada4a"
+}
+
+variable "dbusername"{
+default="rose"
+}
+
+variable "pswrd"{
+default="lavender1"
+}
