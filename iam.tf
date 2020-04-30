77f2c710c7e6665deea5b0d94a988f17416cded6
dhanushka92 <54582030+dhanushka92@users.noreply.github.com>
2020-04-30 17:28:24 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -28,3 +28,8 @@ resource "aws_iam_policy_attachment" "test-attach" {
   roles      = ["${aws_iam_role.test_role.name}"]
   policy_arn = "${aws_iam_policy.policy.arn}"
 }
+resource "aws_iam_policy" "policy" {
+  name        = "test-policy"
+  description = "A test policy"
+  policy      = "${file("policys3bucket.json")}"
+}
