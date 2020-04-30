4faa2fcb2fe316042680c32516ec44ed3e8845e7
dhanushka92 <54582030+dhanushka92@users.noreply.github.com>
2020-04-30 17:19:24 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,7 @@ ami="${var.ami}"
 instance_type="${var.inst_type}"
 subnet_id="${aws_subnet.subnet1.id}"
 vpc_security_group_ids = ["${aws_security_group.sgrp1.id}"]
+iam_instance_profile = "${aws_iam_instance_profile.test_profile.name}"
 user_data = "${file("chrome.ps1")}"
 tags = {
     name = "INSTNCE1"
@@ -15,6 +16,7 @@ ami="${var.ami}"
 instance_type="${var.inst_type}"
 subnet_id="${aws_subnet.subnet2.id}"
 vpc_security_group_ids = ["${aws_security_group.sgrp1.id}"]
+iam_instance_profile = "${aws_iam_instance_profile.test_profile.name}"
 tags = {
     name = "INSTANCE2"
   }
