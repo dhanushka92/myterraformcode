698352056527294b81e9c7bd5d92bfbf73318a26
dhanushka92 <54582030+dhanushka92@users.noreply.github.com>
2020-04-30 17:38:45 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,35 +1,47 @@
-resource "aws_iam_instance_profile" "test_profile" {    
-name  = "test_profile"                         
-roles = ["${aws_iam_role.test_role.name}"]
-}
-
 resource "aws_iam_role" "test_role" {
   name = "test_role"
 
   assume_role_policy = <<EOF
 {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "s3:*",
-            "Resource": [
-                "arn:aws:s3:::resourcepkgs",
-                "arn:aws:s3:::resourcepkgs/*"
-            ]
-        }
-    ]
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
 }
 EOF
 
+  tags = {
+      tag-key = "tag-value"
+  }
 }
-resource "aws_iam_policy_attachment" "test-attach" {
-  name       = "test-attachment"
-  roles      = ["${aws_iam_role.test_role.name}"]
-  policy_arn = "${aws_iam_policy.policy.arn}"
+resource "aws_iam_instance_profile" "test_profile" {
+  name = "test_profile"
+  role = "${aws_iam_role.test_role.name}"
 }
-resource "aws_iam_policy" "policy" {
-  name        = "test-policy"
-  description = "A test policy"
-  policy      = "${file("policys3bucket.json")}"
+resource "aws_iam_role_policy" "test_policy" {
+  name = "test_policy"
+  role = "${aws_iam_role.test_role.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
 }
+EOF
+}
+ 
